901d76533533e31fa73282669bea002ee72b2270
suryausman313-cell <suryausman313@gmail.com>
2025-12-06 00:38:47 +0400

    Update login.js

--- login.js
+++ login.js
@@ -2,11 +2,11 @@ function login() {
     const user = document.getElementById("email").value.trim();
     const pass = document.getElementById("password").value.trim();
 
-    const ADMIN_USER = "admin@123";
+    const ADMIN_USER = "Admin@313"; 
     const ADMIN_PASS = "246800";
 
     if (user === ADMIN_USER && pass === ADMIN_PASS) {
-        localStorage.setItem("logged", "yes");
+        localStorage.setItem("adminLogged", "yes");
         window.location.href = "admin.html";
     } else {
         alert("Wrong Email or Password!");
